0b9b7e00fa5c69c104f05ddffd8f027947cd2375
Kenneth Chyzewski <KChyzewski@gmail.com>
2021-04-24 20:56:52 -0400

    redo

---
+++ js/code.js
@@ -0,0 +1,14 @@
+/* JS Library usage examples */
+"use strict";
+console.log('code.js loaded')
+
+document.addEventListener("keydown", keyDown, false);
+
+window.onload = function() {
+
+}
+
+function keyDown(e) {
+
+}
+
